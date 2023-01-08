f119e1e79fbca864a863b8a5c7b39002e7f9e716
DenisFrolkov <99652468+DenisFrolkov@users.noreply.github.com>
2023-01-08 11:16:30 +0300

    Первое задание

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 fun main() {
-    println("Разработать программу, которая для заданной строки s:\n" +
-            "1) подсчитывает общее число вхождений символов х и у.")
+    taskOne()
+}
+
+fun taskOne(){
+    //1.9.подсчитывает количество цифр в строке;
+    println("С помощью count ")
+    val s = "Денис 18 лет, лентяй 5 ранга".count { it.isDigit() }
+    println(s)
+    println("С помощью filter ")
+    val s1 = "Денис 18 лет, лентяй 5 ранга".filter { it.isDigit() }
+    var counter = 0
+    for (c in s1) {
+        counter++
+    }
+    println(counter)
 }
