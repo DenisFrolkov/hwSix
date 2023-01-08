dc250754d426ba4ff5af6e0451c6e6af7eb54d98
DenisFrolkov <99652468+DenisFrolkov@users.noreply.github.com>
2023-01-08 11:13:19 +0300

    Первое задание

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
-fun main(args: Array<String>) {
+fun main() {
+    taskOne()
+}
 
+fun taskOne(){
+    //1.9.подсчитывает количество цифр в строке;
+    println("С помощью count ")
+    val s = "Денис 18 лет, лентяй 5 ранга".count { it.isDigit() }
+    println(s)
+    println("С помощью filter ")
+    val s1 = "Денис 18 лет, лентяй 5 ранга".filter { it.isDigit() }
+    var counter = 0
+    for (c in s1) {
+        counter++
+    }
+    println(counter)
 }
